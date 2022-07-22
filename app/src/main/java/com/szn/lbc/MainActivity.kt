9cfab710b0970283a25acfadd4503dcb331fc66e
Szn <julien.sezn@gmail.com>
2022-07-22 11:45:39 +0200

    Initial commit

---
+++ app/src/main/java/com/szn/lbc/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.szn.lbc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.szn.lbc.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
